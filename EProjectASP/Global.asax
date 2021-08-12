05b5bb7dd8faaac6f2658d7a65ab112a850c3afe
viet anh nguyen <nguyenvietanh262001@gmail.com>
2021-08-12 16:18:37 +0700

    First commit

---
+++ EProjectASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EProjectASP.MvcApplication" Language="C#" %>
---
+++ EProjectASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EProjectASP.Startup))]
+namespace EProjectASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
